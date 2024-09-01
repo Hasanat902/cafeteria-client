095457d16c69237f92d3d1a48c1e0492a1c5daf7
Hasanat902 <abulhasanat902@gmail.com>
2024-09-01 10:42:09 +0600

    added dynamic title by react helmet

---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import { Helmet } from "react-helmet-async";
+
+
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Cafeteria | Menu</title>
+            </Helmet>
+            <h2>This is our menu</h2>
+        </div>
+    );
+};
+
+export default Menu;
