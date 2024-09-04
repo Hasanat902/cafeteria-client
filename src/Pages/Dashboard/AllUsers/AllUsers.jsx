dada23b597c8e6cf87ee7254b7ca40d291dd843c
Hasanat902 <abulhasanat902@gmail.com>
2024-09-04 12:04:04 +0600

    implement users deleted functionality

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,23 +1,87 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
-    const axiosSecure = useAxiosSecure();
+  const axiosSecure = useAxiosSecure();
 
-    const {data: users = []} = useQuery({
-        queryKey: ['users'],
-        queryFn: async () => {
-            const res = await axiosSecure.get('/users');
-            return res.data;
+  const { data: users = [], refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
+
+  const handleMakeAdmin = user => {
+    
+  }
+
+  const handleDelete = user => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          
+          axiosSecure.delete(`/users/${user._id}`)
+          .then(res => {
+            if(res.data.deletedCount > 0) {
+              refetch();
+              Swal.fire({
+                title: "Deleted!",
+                text: "User has been deleted.",
+                icon: "success"
+              });
+            }
+          })
         }
-    })
+      });
+  };
 
-    return (
-        <div>
-            <h2 className="text-2xl">All Users: {users.length}</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-2xl">All Users: {users.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                users.map((user, index) => <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                    <button onClick={() => handleMakeAdmin(user)} className="btn bg-orange-400 btn-sm"><FaUsers className="text-white
+                    "></FaUsers></button>
+                </td>
+                <td>
+                    <button onClick={() => handleDelete(user)} className="btn btn-ghost btn-lg"><FaTrashAlt className="text-red-600
+                    "></FaTrashAlt></button>
+                </td>
+              </tr>)
+            }
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;
