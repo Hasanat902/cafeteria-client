f801edde528292525a3a4bf3134bccc4549cceaf
Hasanat902 <abulhasanat902@gmail.com>
2024-09-05 12:51:30 +0600

    show all menu items in manage items

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -12,6 +12,7 @@ import Cart from "../Pages/Dashboard/Cart/Cart";
 import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
 import AddItems from "../Pages/Dashboard/AddItems/AddItems";
 import AdminRoute from "./AdminRoute";
+import ManageItems from "../Pages/Dashboard/ManageItems/ManageItems";
 
 export const router = createBrowserRouter([
   {
@@ -62,6 +63,10 @@ export const router = createBrowserRouter([
       {
         path: '/dashboard/addItems',
         element: <AdminRoute><AddItems></AddItems></AdminRoute>
+      },
+      {
+        path: '/dashboard/manageItems',
+        element: <AdminRoute><ManageItems></ManageItems></AdminRoute>
       }
     ]
   }
